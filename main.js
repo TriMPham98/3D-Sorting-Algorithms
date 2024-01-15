82bf795b0ea6c21a7ac37b66bc406230a0677463
Tri <trimpham98@gmail.com>
2024-01-14 16:22:23 -0800

    Add TODO comment

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 // TODO: Create environment
 // TODO: Add blocks of random height in a straight line
-// TODO: Animate the movement ot blocks swapping
+// TODO: Animate the movement of blocks swapping
 // TODO: Add sound effects for swapping action
 // TODO: Add color coding for each type of block: un-swapped, currently swapping, finished swapped
 // TODO: Link environment file to algorithms file
-// TODO: Add bubble sort
+// TODO: Add bubble, selection, insertion, merge, quicksort
 // TODO: Host website
